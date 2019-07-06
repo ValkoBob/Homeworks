b2d2cd30560eb0efe27d1c87817caa8066269eba
Valentyn Bobyr <vbobyr@vob.com>
2019-07-06 20:15:42 +0300

    Made scroll to Top and animation for scrolling from header.

--- WD_PS3_jQuery/Landing/script.js
+++ WD_PS3_jQuery/Landing/script.js
@@ -1,12 +1,31 @@
-$(function() {
-    $(window).scroll(function() {
-        if($(this).scrollTop() !== 0) {
-            $('#toTop').fadeIn();
+$(document).ready(function () {
+    let scrollToTop = $(".scrollToTop");
+    scrollToTop.on('click', function(){
+        $('html, body').animate({scrollTop: 0}, 'slow');
+    });
+
+    $(window).on('scroll', function () {
+        let self = $(this),
+            height = self.height(),
+            top = self.scrollTop();
+        if (top < height) {
+            if (!scrollToTop.hasClass('hidden')) {
+                scrollToTop.addClass('hidden');
+            }
         } else {
-            $('#toTop').fadeOut();
+            scrollToTop.removeClass('hidden');
         }
     });
-    $('#toTop').click(function() {
-        $('body,html').animate({scrollTop:0},800);
+    $('a[href^="#"]').click(function(e) {
+        let element = $($.attr(this, 'href')),
+            elementOffset = element.offset().top,
+            elementHeight = element.height(),
+            windowHeight = $(window).height();
+        if(elementHeight < windowHeight && elementOffset > windowHeight / 2){
+            elementOffset = elementOffset - (windowHeight - elementHeight) / 2;
+        }
+        $('html').animate({
+            scrollTop: elementOffset
+        }, 'slow');
     });
 });
