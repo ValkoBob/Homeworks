e15d2aa81c7a3fbf96a301b2fd58fb291d27f550
Valentyn Bobyr <vbobyr@vob.com>
2019-07-06 17:29:41 +0300

    Make some changes in DropdownBox

--- WD_PS3_jQuery/DropdownBox/script.js
+++ WD_PS3_jQuery/DropdownBox/script.js
@@ -1,15 +1,15 @@
 const CHARACTERS = [
     {
         name: "Jenny Hess",
-        src: "./images/Jenny_Hess.png"
+        src: "./images/Jenny Hess.png"
     },
     {
         name: "Elliot Fu",
-        src: "./images/Elliot_Fu.png"
+        src: "./images/Elliot Fu.png"
     },
     {
         name: "Stevie Feliciano",
-        src: "./images/Stevie_Feliciano.png"
+        src: "./images/Stevie Feliciano.png"
     },
     {
         name: "Christian",
@@ -25,7 +25,7 @@ $(document).ready(() => {
     enableSelector();
 });
 
-function enableSelector(){
+function enableSelector() {
     let options = $('.options');
     let li = '<li></li>';
     CHARACTERS.forEach(function (item) {
@@ -33,18 +33,25 @@ function enableSelector(){
         options.append($(li).attr('value', name)
             .html(`<img src="${item.src}" alt="${name}"><label>${name}</label>`));
     });
-    $('.options li').on('click', function() {
+    $('.options li').on('click', function () {
         let name = $(this).text();
         $('.first_block').css('color', 'black').html(`<img src="images/${name}.png" alt="${name}">
 <label>${name}</label>`);
         $('.options').removeClass('show');
     });
 
-    $('.first_block').on('click', function() {
+    $('.first_block').on('click', function () {
         $('.options').toggleClass('show');
     });
 
-    $('.options li').on('mouseover mouseout', function () {
+    $('.options li').hover(function () {
         $(this).toggleClass('focused');
+    });
+
+    $('body').click(function (e) {
+        if (!$(e.target).hasClass('first_block') && $('ul').hasClass('show')) {
+            $('.options').removeClass('show');
+            console.log("yes!")
+        }
     });
 }
