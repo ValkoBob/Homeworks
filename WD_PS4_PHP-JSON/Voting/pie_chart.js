b364e6e9595ff2ef0ef3c45cc409ea8dba27eb53
Valko <Valko05081994>
2019-08-30 18:35:40 +0300

    Make voting.

---
+++ WD_PS4_PHP-JSON/Voting/pie_chart.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $.getJSON('date.json', function (votingData) {
+    google.charts.load('current', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      let table = [['Option number', 'value']];
+      for(let key in votingData['voting']){
+        if(votingData['voting'].hasOwnProperty(key)){
+          let element = [key, votingData['voting'][key]];
+          table.push(element);
+        }
+      }
+      let data = google.visualization.arrayToDataTable(table);
+      let options = {
+        title: 'Most popular Steel Hunter.'
+      };
+      let chart = new google.visualization.PieChart(document.getElementById('pie_chart'));
+      chart.draw(data, options);
+    }
+  });
+});
---
+++ WD_PS4_PHP-JSON/Voting/result.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Result of voting</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .pie_chart{
+            width: 90vh;
+            height: 50vh;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <a href="index.php"> << Back</a>
+</div>
+<div class="pie_chart" id="pie_chart"></div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript" src="pie_chart.js"></script>
+</body>
+</html>
